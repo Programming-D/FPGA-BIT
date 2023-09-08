31f08612d3bd1c5d411caa97ef6b31990feb5610
root <bit.ruixini@gmail.com>
2023-09-08 17:31:59 +0800

    last

--- cifar_convert.py
+++ cifar_convert.py
@@ -1,11 +1,5 @@
-import numpy as np
-import os
 import torch
-from torchvision.datasets import mnist
-from torch.nn import CrossEntropyLoss
-from torch.optim import SGD, Adam
 from torch.utils.data import DataLoader
-from torchvision.transforms import ToTensor
 from torchvision.transforms import transforms
 from torchvision.datasets import CIFAR10
 transform_test = transforms.Compose([
